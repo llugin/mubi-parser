92b41e08ea1fd933d821ccc302063aba77c1d2f0
Leszek Lugin <leszek.lugin@intel.com>
2018-07-29 23:25:31 +0200

    Add reading/writing data as json

--- mubi.go
+++ mubi.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -24,14 +29,16 @@ const (
 	selDuration       = "[itemprop=duration]"
 )
 
+var cacheFile = filepath.Join(os.Getenv("GOPATH"), "mubi.json")
+
 type movieData struct {
-	title             string
-	director          string
-	country           string
-	year              string
-	duration          string
-	mubiRating        string
-	mubiRatingsNumber string
+	Title             string `json:"title"`
+	Director          string `json:"director"`
+	Country           string `json:"country"`
+	Year              string `json:"year"`
+	Duration          string `json:"duration"`
+	MubiRating        string `json:"MUBI rating"`
+	MubiRatingsNumber string `json:"MUBI ratings num"`
 }
 
 func queryMovieDetails(url string, md *movieData) {
@@ -46,10 +53,10 @@ func queryMovieDetails(url string, md *movieData) {
 		log.Fatal("Error loading HTTP response body. ", err)
 	}
 
-	md.mubiRating = strings.TrimSpace(document.Find(selRating).Text())
-	md.duration = strings.TrimSpace(document.Find(selDuration).Text())
+	md.MubiRating = strings.TrimSpace(document.Find(selRating).Text())
+	md.Duration = strings.TrimSpace(document.Find(selDuration).Text())
 	raw := document.Find(selRatingsNumber).Text()
-	md.mubiRatingsNumber = strings.TrimSpace(strings.Trim(raw, "Ratings\n"))
+	md.MubiRatingsNumber = strings.TrimSpace(strings.Trim(raw, "Ratings\n"))
 }
 
 func queryMovies(doc *goquery.Document) []movieData {
@@ -58,12 +65,12 @@ func queryMovies(doc *goquery.Document) []movieData {
 	doc.Find(selMovie).Each(func(i int, s *goquery.Selection) {
 		var md movieData
 
-		md.title = s.Find(selTitle).Text()
-		md.director = s.Find(selDirector).Text()
+		md.Title = s.Find(selTitle).Text()
+		md.Director = s.Find(selDirector).Text()
 
 		countryAndYear := strings.Split(s.Find(selCountryAndYear).Text(), ", ")
-		md.country = countryAndYear[0]
-		md.year = countryAndYear[1]
+		md.Country = countryAndYear[0]
+		md.Year = countryAndYear[1]
 
 		link, exists := s.Find(selLink).Attr("href")
 		if !exists {
@@ -73,26 +80,59 @@ func queryMovies(doc *goquery.Document) []movieData {
 		time.Sleep(time.Second * 3)
 
 		queryMovieDetails(baseURL+link, &md)
-		fmt.Printf("%+v\n", md)
 		movies = append(movies, md)
 	})
 
 	return movies
 }
 
-func main() {
+func readFromCached() []movieData {
+	out, err := ioutil.ReadFile(cacheFile)
+	if err != nil {
+		log.Fatal("cache file could not be opened ", err)
+	}
+	var movies []movieData
+	if err := json.Unmarshal(out, &movies); err != nil {
+		log.Fatal("Could not read data from cache ", err)
+	}
+	return movies
+}
+
+func readFromWebPage() []movieData {
 	resp, err := http.Get(showingURL)
 	if err != nil {
-		log.Fatal("Error loading html body", err)
+		log.Fatal("Error loading html body ", err)
 	}
 	defer resp.Body.Close()
-
 	time.Sleep(time.Second * 3)
-
 	document, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Fatal("Error loading HTTP response body. ", err)
 	}
+	return queryMovies(document)
+}
 
-	queryMovies(document)
+func main() {
+	fromFile := flag.Bool("cached", false, "Read data from mubi.json file")
+	flag.Parse()
+
+	var movies []movieData
+
+	if *fromFile {
+		movies = readFromCached()
+	} else {
+		movies = readFromWebPage()
+	}
+
+	out, err := json.MarshalIndent(movies, "", " ")
+	if err != nil {
+		log.Fatal("json could not be marshalled ", err)
+	}
+
+	fmt.Println(string(out))
+
+	err = ioutil.WriteFile(cacheFile, out, 0666)
+	if err != nil {
+		log.Fatal("Could not write to mubi.json file ", err)
+	}
 }
