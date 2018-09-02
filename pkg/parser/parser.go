a524bcabac6dcd765ff9b99f5d1db968584941ce
Leszek Lugin <leszek.lugin@intel.com>
2018-09-02 21:11:51 +0200

    use merge pattern to handle channels

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"github.com/llugin/mubi-parser/pkg/movie"
 	"github.com/llugin/mubi-parser/pkg/mubi"
 	"log"
+	"sync"
 )
 
 // GetMovies reads movie data from the web
@@ -16,31 +17,33 @@ func GetMovies(refresh bool) ([]movie.Data, error) {
 		return movies, err
 	}
 
+	cached := make(chan movie.Data, mubi.MaxMovies)
 	if !refresh {
-		cached, err := movie.ReadFromCached()
-		if err != nil {
-			log.Printf("%v. Could not read cached data, reading from web", err)
-		} else {
-			var found <-chan movie.Data
-			out, found = channelCachedDetails(cached, out)
-			for m := range found {
-				movies = append(movies, m)
-			}
-		}
+		out, cached = channelCachedDetails(out)
+	} else {
+		close(cached)
 	}
 	out = mubi.ReceiveMoviesDetails(out)
 	out = imdb.GetRatings(out)
 
-	for m := range out {
+	for m := range merge(out, cached) {
 		movies = append(movies, m)
 	}
 	log.Printf("OMDB API called %v times\n", imdb.APICount)
 	return movies, nil
 }
 
-func channelCachedDetails(vals []movie.Data, in <-chan movie.Data) (<-chan movie.Data, <-chan movie.Data) {
-	new := make(chan movie.Data, mubi.MaxMovies)
+func channelCachedDetails(in <-chan movie.Data) (<-chan movie.Data, chan movie.Data) {
+	vals, err := movie.ReadFromCached()
+
 	cached := make(chan movie.Data, mubi.MaxMovies)
+	if err != nil {
+		log.Printf("%v. Could not read cached data, reading from web", err)
+		close(cached)
+		return in, cached
+	}
+
+	new := make(chan movie.Data, mubi.MaxMovies)
 	go func() {
 		for md := range in {
 			if val, found := find(md, vals); found == true {
@@ -63,4 +66,27 @@ func find(searched movie.Data, in []movie.Data) (movie.Data, bool) {
 	}
 	var empty movie.Data
 	return empty, false
+}
+
+// taken from https://blog.golang.org/pipelines
+func merge(cs ...<-chan movie.Data) <-chan movie.Data {
+	var wg sync.WaitGroup
+	out := make(chan movie.Data)
+
+	output := func(c <-chan movie.Data) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
